b3f3d7853671386b84fc09237064d34f9ce7a585
Gulnaz-Akhmadieva <gulnaz.akhmadieva@internet.ru>
2026-02-26 16:57:00 +0500

    Замена части кода на явные ожидания в task2

--- 06_lesson/lesson06_task2.py
+++ 06_lesson/lesson06_task2.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -18,8 +19,10 @@ search.send_keys("SkyPro")
 print("Текст SkyPro введен")
 
 
-# находим и кликаем синюю кнопку
-blue_button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
+# ждем, пока кнопка появится И СРАЗУ КЛИКАЕМ
+blue_button = wait.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary"))
+)
 blue_button.click()
 print("Клик по синей кнопке выполнен")
 
